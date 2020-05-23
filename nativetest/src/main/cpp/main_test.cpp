a2a5164fc3595367f57660d67944363683cc0bda
Amos Lim <amoseui@gmail.com>
2020-05-24 00:01:22 +0900

    WIP: Native Test

---
+++ nativetest/src/main/cpp/main_test.cpp
@@ -0,0 +1,20 @@
+#include "gtest/gtest.h"
+
+/*
+TEST(SampleTest, AssertionTrue) {
+    ASSERT_EQ(1, 1);
+}
+
+TEST(SampleTest, TestSampleMethod) {
+    ASSERT_EQ(1, test());
+}
+*/
+
+TEST(SampleTest, TestSampleMethod) {
+    ASSERT_EQ(1, 1);
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
